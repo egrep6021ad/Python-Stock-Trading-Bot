d27c259be8f8278fba9bf0d22995f94cd0cd1bb5
Khalfani Bozeman <84476080+egrep6021ad@users.noreply.github.com>
2022-04-10 18:01:16 +0000

    apr 10 2:01 Comments and functions added

--- runner_algo.py
+++ runner_algo.py
@@ -1,110 +1,118 @@
 import math
-def runner_algo(candle_stick, # Dictionary with TCKR history
-                today,        # Date
-                minimum_days_momentum, # Min days trending to purchase 
-                input_dollars          # Money per asset
-               ):
+def runner_algo(candle_stick,           # Dictionary with TCKR history
+                today,                  # Date
+                min_momentum,           # Min days trending to purchase 
+                input_dollars ):        # Money per asset
+                 
   with open(f'./Quick/{today[5:]} candle_read.txt', 'w+') as fst2:
-    breakout = 0 
-    last_high = 0
-    breakout = candle_stick.get('1')# The first element will set..
-    breakout = float(breakout[2]) # resistance / base - line
-    count = 0                     # Track days to see when min. is satisfied
-    bool = 0                      # Determines which direction data is trending
-    k = ""                        # Track when SYMBOL changes
-    dollars_dict = {}             # Track returns per asset
-
+    prev_day_high = 0
+    support = float(candle_stick.get('0')[2])  # The first element's LOW = ENTRY point
+    days_trending = 0                        # Track days to see when min. is satisfied
+    invested = False                      # Determines which direction data is trending
+    prev_symbol = ""                        # Track when SYMBOL changes
+    portfolio = {}                     # Track returns per asset
+    daily_high_arr = []
+    shares = 1
+   
+    # Starting funds:
+    funds_remain = input_dollars
+    end = list(candle_stick.items())[-2][1][0]
+   
     # Iterate though every entry ("Candle Stick")
-    for (key,value) in candle_stick.items():
-      t = value[3]  # Track when the SYMBOL (asset) changes
-      if t != k:
-        k = value[3]
-        dollars = input_dollars  # If it changes, allot it's full ammount
-      try:
-        if  float(value[1]) >= breakout:     # Daily high > resistance
-          if  float(value[1]) > last_high:   # Daily high > previous high
-            count += 1                       # Trending...
-            if count >= minimum_days_momentum:  # If min. days trending = satisfied
-              breakout = float(value[1])        # Set new resistance / base-line
-
-              # As soon as min. momentum is met & there is enough money -> BUY
-              if count == minimum_days_momentum:  
-                if bool == 0 and float(value[1])<= dollars:
-                  # Change direction  
-                  bool = 1
-                  fst2.write(f"\nBUY {value[3]}! --> ({count} Day Increase):\n")
-                  fst2.write(f'Current Days High: ${value[1]}\n')
-                  # Calculate how many shares can be purchased
-                  multiplier = math.floor(dollars / float(value[1]))
-                  # If at lease 1 share can be bought...
-                  if multiplier > 1:
-                    dollars = dollars - (float(value[1]) * multiplier)
-                  
-                  else:
-                    # Since rounding down it could be 0, so set as 1 & check again...
-                    multiplier = 1
-                    # If one share can be bought, buy it
-                    if float(value[1]) < dollars:
-                      dollars = dollars - (float(value[1]) * multiplier)
-                  print(f"\nAfter Buy: {dollars:0.2f} ({value[3]})")
-                  print(f"\tPrice per share: {value[1]}")
-                  print(f"\tShares: {multiplier}")
+    for (key,value) in list(candle_stick.items())[1:-1]:
+      
+      # Get Symbol & current day HIGH
+      symbol = value[3]  
+      daily_high_arr.append(float(value[1]))
+      curr_high = daily_high_arr[-1]
+      
+      # If holding on the most recent day, SELL
+      if invested == True and (symbol != prev_symbol or value[0] == end ):
+        with open(f'./Quick/{today[5:]} candle_read.txt', 'a+') as f:
+            f.write("append")
+            f.write(f"\tSELL: {value[3]} @ ${curr_high}\n")
+            funds_remain = funds_remain + curr_high * shares
+            f.write(f"\t\t{value[4]}\n")
+            change_perc =  get_dec_percentage(curr_high,prev_day_high)
+            f.write(f'\t\t<----- Decreasing\n')
+            f.write(f'\t\tChange from support line (swing low): -{change_perc:0.2f}%\n\n')
+            portfolio.update({value[3]:funds_remain}) 
+          
+      # SYMBOL (asset) CHANGE:
+      if symbol != prev_symbol:
+        prev_symbol = value[3]
+        funds_remain = input_dollars
+        support = 0
+     
+        
+      # START:
+      if curr_high >= support:     
+        if curr_high > prev_day_high:   
+          days_trending += 1         
+          if days_trending >= min_momentum:  
+            support = curr_high 
+            
+            # BUY soon as min. momentum is met & there is enough money
+            if days_trending == min_momentum:  
+                if invested == False and curr_high<= funds_remain:
+                  asset = buy_asset(today,value,curr_high,days_trending,funds_remain,invested)
+                  invested = True
+                  funds_remain = asset[0]
+                  shares = asset[1]
                 fst2.write(f"{value[4]}\n")
-                change_perc = get_inc_percentage(value[1],last_high)
+                change_perc = get_inc_percentage(curr_high,prev_day_high)
                 fst2.write(f'Percentage Change +{change_perc:0.2f}%\n')
-              # Holding (post purchase)
-              else:
-                fst2.write(f"\nHOLD Day: {str(count-minimum_days_momentum)} ({str(count)} Days Since Buy Indicator)\n")
-                change_perc = get_dec_percentage(value[1],last_high)
-                change_perc = change_perc * -1
-                fst2.write(f"Percent Change: {change_perc :0.2f}%\n")
-              fst2.write(f'Previous Days High: {last_high}\n\n')
-            # Last high is always previous day!             
-            last_high = float(value[1])
-          # Otherwise, were not trending up, but above resistance  
-          else:
-            count = 0
-            last_high = breakout
-        # Broke throuh resistance
-        elif float(value[1]) < breakout:
-          change_perc =  get_dec_percentage(value[1],last_high)
-          breakout = float(value[1])
-          count = 0
-          # If we have assets, and weve fallen ___% far...SELL
-          if bool == 1 and change_perc>0.5: # working KEY ONE! This stops from small changes
-            bool = 0
-            fst2.write(f"\tSELL: {value[3]} @ ${value[1]}\n")
-            dollars = dollars + (float(value[1]) * multiplier)
-            print(f"After Sell: {dollars:0.2f} ({value[3]})")
-            print(f"\tPrice per share: {value[1]}")
-            print(f"\tShares: {multiplier}\n")
+            # HOLD
+            else:
+              fst2.write(f"\nHOLD Day: {str(days_trending-min_momentum)}({str(days_trending)}Since Buy Indicator)\n")
+              change_perc = get_dec_percentage(curr_high,prev_day_high)
+              change_perc = change_perc * -1
+              fst2.write(f"Percent Change: {change_perc :0.2f}%\n")
+            fst2.write(f'Previous Days High: {prev_day_high}\n\n')
+                      
+          prev_day_high = curr_high
+        # Otherwise, were not trending up, but above resistance  
+        else:
+          days_trending = 0
+          prev_day_high = support
+          
+      # Fell throuh support
+      elif curr_high < support:
+          change_perc =  get_dec_percentage(curr_high,prev_day_high)
+          support = curr_high
+          days_trending = 0
+          # SELL! assets have fallen ___% far...
+          if invested == True and change_perc>0: # This stops from small changes
+            invested = False
+            fst2.write(f"\tSELL: {value[3]} @ ${curr_high}\n")
+            funds_remain = funds_remain + curr_high * shares
           fst2.write(f"\t\t{value[4]}\n")
-          change_perc =  get_dec_percentage(value[1],last_high)
+          change_perc =  get_dec_percentage(curr_high,prev_day_high)
           fst2.write(f'\t\t<----- Decreasing\n')
           fst2.write(f'\t\tChange from support line (swing low): -{change_perc:0.2f}%\n\n')
-         
-      except ValueError:
-         print("Value error")
-         continue
-      except TypeError:
-         print("Type error")
-         continue
-      dollars_dict.update({value[3]:dollars})
+      # update { $SYM : New Balance afer transaction}
+      portfolio.update({value[3]:funds_remain})
+
+  # END: Cumulative data ->  txt file
       
+  with open(f'./Quick/{today[5:]} candle_read.txt', 'a') as fst2:     
   # Print The statistics
-  temp = 0
-  temp2 = 0
-  print(f"\nStart: $ {input_dollars} Each TICKR")
-  for (key,value) in dollars_dict.items():
-    print(f'\t{key}: $ {value:0.2f}')
-    temp += float(value)
-    temp2 += 1
-  print(f"\nInvested: {str(input_dollars*temp2)}")  
-  print(f"Returned: {temp:0.2f}")
-  print(f"Profit: $ {temp-input_dollars*temp2:0.2f}")
-  change_perc = get_dec_percentage(temp,(input_dollars * temp2))
-  change_perc = change_perc * -1
-  print(f"Change: {change_perc:0.2f} %\n")
+    cash_out=0
+    cash_in = 0
+    fst2.write(f"\nStart: $ {input_dollars} Each symbol\n")
+    for (symbol,dollar_value) in portfolio.items():
+      fst2.write(f'\t{symbol}: $ {dollar_value:0.2f}\n')
+      cash_out += float(dollar_value)
+      print(float(dollar_value))
+      cash_in += 1
+    fst2.write(f"\nInvested: {str(input_dollars*cash_in)}\n")  
+    fst2.write(f"Returned: {cash_out:0.2f}\n")
+    fst2.write(f"Profit: $ {cash_out-input_dollars*cash_in:0.2f}\n")
+    change_perc = get_dec_percentage(cash_out,(input_dollars * cash_in))
+    change_perc = change_perc * -1
+    print(f"Change: {change_perc:0.2f} %\n")
+    fst2.write(f"Change: {change_perc:0.2f} %\n")
+    print(portfolio)
   return
 
 # Helper function
@@ -117,3 +125,18 @@ def get_dec_percentage(a,b):
   a = float(a)
   b = float(b)
   return ((b-a)/abs(b) ) * 100
+
+#Helper Function
+def buy_asset(today,value,curr_high,days_trending,funds_remain,invested):
+  with open(f'./Quick/{today[5:]} candle_read.txt', 'a') as fst2:
+    #invested = True
+    fst2.write(f"\nBUY {value[3]}! --> ({days_trending} Day Increase):\n")
+    fst2.write(f'Current Days High: ${curr_high}\n')
+    # Calculate how many shares can be purchased
+    multiplier = math.floor(funds_remain / float(curr_high))
+    # If at lease 1 share can be bought...
+    if multiplier > 1:
+      funds_remain = funds_remain - curr_high * multiplier
+    else:
+      funds_remain = funds_remain - curr_high
+  return (funds_remain,multiplier)

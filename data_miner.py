15510aee42f8df0765dc870e4ee92eac0b1f2621
Khalfani Bozeman <84476080+egrep6021ad@users.noreply.github.com>
2022-06-05 18:48:33 +0000

    5 June 2022 Key Error in "stock_trader.py"

--- data_miner.py
+++ data_miner.py
@@ -25,8 +25,9 @@ if __name__ == "__main__":
   try:
     # Returns array of SYMBOLS for days biggest movers:
     arr = getMovers(poop)
+    arr = ['APA','MRO','TSLA','NFLX']
     pprint(arr)
-    #arr = ['APA','MRO','TSLA','NFLX']
+   
     time.sleep(10)
     print("40 Seconds.")
     # Get Quotes for all of those big movers:
@@ -48,7 +49,7 @@ if __name__ == "__main__":
     print(temp)
       
     #Get Options:
-    getOptions('TSLA',poop)
+    #getOptions('TSLA',poop)
   except KeyError:
     print("Key Error!")
   print("DONE")
